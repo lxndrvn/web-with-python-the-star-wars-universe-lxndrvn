45b5c7792362f269cc7f36fb43e2732efe831a51
lxndrvn <i1990522@gmail.com>
2017-04-03 23:30:28 +0200

    add code for retrieving planet data

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import *
+import requests
+
+app = Flask(__name__, template_folder="templates", static_url_path="/static",
+            static_folder="static")
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def planets():
+    planets_data = requests.get('http://swapi.co/api/planets')
+    planets = planets_data.json()['results']
+    
+    for planet in planets:
+        residents = planet['residents']
+        resident_count = len(residents)
+        planet['resident_count'] = resident_count
+
+    previous_page = planets_data.json()['previous']
+    next_page = planets_data.json()['next']
+
+    return render_template(
+        'home.html', planets=planets, previous=previous_page, next=next_page
+    )
+
+
+if '__main__' == __name__:
+    app.run(debug=True)
